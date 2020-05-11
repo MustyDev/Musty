a79047832d919072008a81e91d5f9758c55f3ab5
bayuiqballl <bayuiqballl13@gmail.com>
2020-05-11 16:17:21 +0700

    improve about

--- front-end/src/Pages/About.js
+++ front-end/src/Pages/About.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import styled from "styled-components";
 import { Jumbotron, Button, Form } from "react-bootstrap";
+import "../App.css";
 import Images from "../Images/Logo-Musty.png";
 
 const About = () => {
@@ -28,14 +29,64 @@ const About = () => {
             <img src={Images} />
           </div>
           <div className="col-md pt-5">
-            <h1>We take care of everything</h1>
-
+            <h1>We take care of music</h1>
+            <hr />
             <h3>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Nulla
               facilisi etiam dignissim diam quis enim. Scelerisque eleifend
               donec pretium vulputate sapi.
             </h3>
+          </div>
+        </div>
+      </section>
+
+      <section className="container-fluid">
+        <div className="container">
+          <div className="row">
+            <div className="col py-5">
+              <h2 className="text-center">
+                <strong>OUR FULL-STACK DEVELOPER TEAM</strong>
+              </h2>
+              <hr />
+              <div className="row mt-5">
+                <div className="col text-center">
+                  <img src alt="" />
+                  <h4>
+                    <strong>Wahid</strong>
+                  </h4>
+                  <p className="text-secondary">Team lead</p>
+                </div>
+                <div className="col text-center">
+                  <img src alt="" />
+                  <h4>
+                    <strong>Hadyd</strong>
+                  </h4>
+                  <p className="text-secondary">Web Designer</p>
+                </div>
+                <div className="col text-center">
+                  <img src alt="" />
+                  <h4>
+                    <strong>Zakiy Dani</strong>
+                  </h4>
+                  <p className="text-secondary">Software Engineer</p>
+                </div>
+                <div className="col text-center">
+                  <img src alt="" />
+                  <h4>
+                    <strong>Enggar</strong>
+                  </h4>
+                  <p className="text-secondary">Software Engineer</p>
+                </div>
+                <div className="col text-center">
+                  <img src alt="" />
+                  <h4>
+                    <strong>Bayu</strong>
+                  </h4>
+                  <p className="text-secondary">Software Engineer</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
