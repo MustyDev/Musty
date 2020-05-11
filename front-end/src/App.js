9c35ad2e8e0da5e4cd4ae5ef8da333e2d0c22d22
MrHide06 <wahid.smkn10@gmail.com>
2020-05-11 13:15:27 +0700

    Add Admin

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,18 @@
 import React from "react";
 
-<<<<<<< HEAD
+
 import TourCharity from './Pages/TourCharity'
 import Admin from './Pages/Admin'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-=======
 import TourCharity from "./Pages/TourCharity";
 import About from "./Pages/About";
 
 import "bootstrap/dist/css/bootstrap.min.css";
->>>>>>> 1c55d0bcef7cbb617317ed96f047695df7f7b478
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Router>
         <Switch>
           <Route exac path="/form-tourcharity">
@@ -24,12 +21,13 @@ function App() {
           <Route path="/admin">
             <Admin /> 
           </Route>
+          <Route path="/about">
+            <About />
+          </Route>
         </Switch>
       </Router>
-=======
-      <About />
+      
       {/* <TourCharity /> */}
->>>>>>> 1c55d0bcef7cbb617317ed96f047695df7f7b478
     </div>
   );
 }
