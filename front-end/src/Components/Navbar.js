ae6ddf2ecbd270ee221ee880bdfdded1ead24717
MrHide06 <wahid.smkn10@gmail.com>
2020-05-18 15:51:36 +0700

    Private Navbar

--- front-end/src/Components/Navbar.js
+++ front-end/src/Components/Navbar.js
@@ -1,63 +1,215 @@
 import React from "react";
+
 import { Navbar } from "react-bootstrap";
+
 import { Link } from "react-router-dom";
+
 import { FiLogIn } from "react-icons/fi";
 import { GrNotes } from "react-icons/gr";
 import { FiInstagram } from "react-icons/fi";
 import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+
+import { useSelector } from 'react-redux';
+
+import jwt from 'jwt-decode';
+
 import "../App.css";
 
 const Nav = () => {
-  return (
-    <>
-      <Navbar className="navbarheader">
-        <a
-          href="https://www.instagram.com/"
-          className="icon-sosmed"
-          target="_blank"
-        >
-          <FiInstagram />
-        </a>
-        <a
-          href="https://www.facebook.com/"
-          className="icon-sosmed"
-          target="_blank"
-        >
-          <FaFacebookF />
-        </a>
-        <a
-          href="https://www.twitter.com/"
-          className="icon-sosmed"
-          target="_blank"
-        >
-          <FaTwitter />
-        </a>
-        <a
-          href="https://github.com/MustyDev"
-          className="icon-sosmed"
-          target="_blank"
-        >
-          <FaGithub />
-        </a>
-
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <GrNotes />
-            <Link to="/form-tourcharity" className="text-donasi">
-              Buat Donasi
-            </Link>
-            <Link to="/login" className="text text-muted">
-              Masuk
-            </Link>
-            <Link to="/register" className="text">
-              Daftar
-            </Link>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Navbar>
-    </>
-  );
+  const isLogged = useSelector((state) => state.loginState.isLogged)
+  
+  if (isLogged === false ) {
+    return (
+      <>
+        <Navbar className="navbarheader">
+          <a
+            href="https://www.instagram.com/"
+            className="icon-sosmed"
+            target="_blank"
+          >
+            <FiInstagram />
+          </a>
+          <a
+            href="https://www.facebook.com/"
+            className="icon-sosmed"
+            target="_blank"
+          >
+            <FaFacebookF />
+          </a>
+          <a
+            href="https://www.twitter.com/"
+            className="icon-sosmed"
+            target="_blank"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="https://github.com/MustyDev"
+            className="icon-sosmed"
+            target="_blank"
+          >
+            <FaGithub />
+          </a>
+  
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <Link to="/login" className="text text-muted">
+                Masuk
+              </Link>
+              <Link to="/register" className="text">
+                Daftar
+              </Link>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Navbar>
+      </>
+    );
+  }else if (isLogged === true){
+    let decode = jwt(localStorage.getItem("token"))
+    if (decode.role === 0){
+      return (
+        <>
+          <Navbar className="navbarheader">
+            <a
+              href="https://www.instagram.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FiInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://www.twitter.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://github.com/MustyDev"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
+    
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <GrNotes />
+                <Link to="/form-tourcharity" className="text-donasi">
+                  Buat Donasi
+                </Link>
+                <Link to="/admin" className="text-donasi">
+                  Admin
+                </Link>
+                <Link to="/logout" className="text text-muted">
+                  LogOut
+                </Link>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Navbar>
+        </>
+      );
+    } else if (decode.role === 1){
+      return (
+        <>
+          <Navbar className="navbarheader">
+            <a
+              href="https://www.instagram.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FiInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://www.twitter.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://github.com/MustyDev"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
+    
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <GrNotes />
+                <Link to="/form-tourcharity" className="text-donasi">
+                  Buat Donasi
+                </Link>
+                <Link to="/logout" className="text text-muted">
+                  LogOut
+                </Link>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Navbar>
+        </>
+      );
+    } else if(decode.role === 2){
+      return (
+        <>
+          <Navbar className="navbarheader">
+            <a
+              href="https://www.instagram.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FiInstagram />
+            </a>
+            <a
+              href="https://www.facebook.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaFacebookF />
+            </a>
+            <a
+              href="https://www.twitter.com/"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaTwitter />
+            </a>
+            <a
+              href="https://github.com/MustyDev"
+              className="icon-sosmed"
+              target="_blank"
+            >
+              <FaGithub />
+            </a>
+    
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <Link to="/logout" className="text text-muted">
+                  LogOut
+                </Link>
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Navbar>
+        </>
+      );
+    }
+  }
+  
 };
 export default Nav;
